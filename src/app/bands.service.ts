68fdc350bfb1f5cb0290d1d65197a0ee6e9581e4
yuvalhoch <yuvalhoch@next-insurance.com>
2020-03-22 18:31:05 +0200

    add band form

--- src/app/bands.service.ts
+++ src/app/bands.service.ts
@@ -1,7 +1,34 @@
-import { Subject } from "rxjs";
+import { Injectable } from '@angular/core';
+import {Observable, of} from "rxjs";
+import {Band} from "./band.model";
 
+@Injectable({
+  providedIn: 'root'
+})
 export class BandsService {
-  addBand = new Subject<number>();
 
+  bands : Band[];
+
+  constructor() {
+    let localStoreBands = localStorage.getItem('bands');
+    if (localStoreBands == null) {
+      this.bands = []
+    } else {
+      this.bands = JSON.parse(localStoreBands);
+    }
+  }
+
+  getBands(): Observable<Band[]> {
+    return of<Band[]>(this.bands);
+  }
+
+  getColumns(): string[] {
+    return ["name", "origin", "yearsActive", "website"]
+  };
+
+  addBand(band: Band) {
+    this.bands.push(band);
+    localStorage.setItem('bands', JSON.stringify(this.bands));
+  }
 
 }
