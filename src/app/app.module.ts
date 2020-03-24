37a2c1eeb2630005f67f45113e0cfaf06a8ccd1c
yuvalhoch <yuvalhoch@next-insurance.com>
2020-03-24 14:42:17 +0200

    Revert "first commit"
    This reverts commit bd55f4bb

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { AddBandComponent } from './add-band/add-band.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    AddBandComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
